f79f2e815c65a707dff29e16a1b2128ddda9fb0f
耳得为声 <wuhaisen403@163.com>
2023-06-18 12:45:12 +0800

    @Configuration和@Bean注解

--- src/main/java/com/whs/edws/controller/HelloController.java
+++ src/main/java/com/whs/edws/controller/HelloController.java
@@ -1,18 +1,24 @@
 package com.whs.edws.controller;
 
 
+import com.whs.edws.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/con")
 public class HelloController {
 
+    @Resource
+    private User user;
+
 
     @GetMapping("/hello")
     public String hello(){
-       return  "Hello SpringBoot!";
+       return  "Hello SpringBoot!" + user;
     }
 
 
---
+++ src/main/java/com/whs/edws/entity/User.java
@@ -0,0 +1,13 @@
+package com.whs.edws.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+}
