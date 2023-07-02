5aba438457032b55579e7887c4161aaacaa930e9
耳得为声 <wuhaisen403@163.com>
2023-07-02 15:29:21 +0800

    南乔到家后端：rabbitMQ配置手动ack

--- src/main/java/com/whs/edws/rabbitmq/HelloWorldConsumer.java
+++ src/main/java/com/whs/edws/rabbitmq/HelloWorldConsumer.java
@@ -1,16 +1,25 @@
 package com.whs.edws.rabbitmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class HelloWorldConsumer {
 
     @RabbitListener(queuesToDeclare = {@Queue(name = "simpleQueue")})
-    public void consumer(String msg){
+    public void consumer(String msg, Channel channel, Message message) throws IOException {
         log.info("simple模式接收到的消息：" + msg);
+
+        // 手动发送确认消息
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 }
